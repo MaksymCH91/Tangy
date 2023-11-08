39a3b7c977dbe07de7e747e60f907f4b412c2686
Чорноконь Максим Олександрович <aimmaxblack@gmail.com>
2023-11-08 22:13:45 +0200

    Добавьте файлы проекта.

---
+++ Tandy_models/LearnBlazorModels/Demoproduct.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tandy_models.LearnBlazorModels
+{
+    public class Demoproduct
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public bool IsActiv { get; set; }
+        public List<Demo_productprops> ProductProperies { get; set; }
+    }
+}
---
+++ TangyWeb_Server/Helper/IJSRuntimeExtention.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.JSInterop;
+
+namespace TangyWeb_Server.Helper
+{
+    public static class IJSRuntimeExtention
+    {
+        public static async ValueTask ToastrSuccess(this IJSRuntime jsRuntime, string message)
+        {
+            await jsRuntime.InvokeVoidAsync("ShowToasrt","Success", message);
+        }
+    }
+}
